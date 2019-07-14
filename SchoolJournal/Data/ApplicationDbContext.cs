27e1c5e279ad0afc0add4e48af013d0704b3e6a0
zsuz <Avdosevstas@mail.ru>
2019-07-14 11:10:40 +0400

    start

---
+++ SchoolJournal/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolJournal.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolJournal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>//: IdentityDbContext<ApplicationUser>
+    {
+        //public DbSet<Article> Articles { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+               : base(options)
+        {
+            Database.EnsureCreated();//создаст БД если ее нет
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // использование Fluent API см по тегу fluent
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
